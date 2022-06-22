48f41fc8c3e74701375ee2d8d1a75a0ef83493d9
Kelopy <theytrank@gmail.com>
2022-06-22 13:07:33 +0100

    fully working ping command

--- src/main/java/me/kelopy/acrobatic/commands/ping.java
+++ src/main/java/me/kelopy/acrobatic/commands/ping.java
@@ -2,6 +2,8 @@ package me.kelopy.acrobatic.commands;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.kelopy.acrobatic.Acrobatic;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,11 +20,24 @@ public class ping implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (sender instanceof Player p){
-            String ping = plugin.getConfig().getString("ping");
-            ping = PlaceholderAPI.setPlaceholders(p, ping);
-            p.sendMessage(ping);
-        } else {
+        if(sender instanceof Player p){
+
+            if(args.length == 0){
+                String ping = plugin.getConfig().getString("ping");
+                ping = PlaceholderAPI.setPlaceholders(p, ping);
+                p.sendMessage(ping);
+            }else{
+                Player target = Bukkit.getPlayer(args[0]);
+                if(target != null){
+                    String pingTarget = plugin.getConfig().getString("ping-target");
+                    pingTarget = PlaceholderAPI.setPlaceholders(target, pingTarget);
+                    p.sendMessage(pingTarget);
+                }else{
+                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cPlayer not found."));
+                }
+            }
+
+        }else{
             System.out.println("This command can only be executed by a player.");
         }
 
